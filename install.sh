2eb114951df94852d37977fb99791875248f6569
TLu <tlu@team-tl.pl>
2023-04-03 23:08:37 +0200

    added timer 30s after reboot

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ if [[ "$EUID" = 0 ]]; then
 else
     sudo -k
     if sudo true; then
-        echo "Sucessfully got root"
+        echo "Successfully got root"
     else
         echo "Wrong password"
         exit 1
@@ -14,16 +14,24 @@ fi
 sudo pip3 install pyautogui
 sudo pip3 install pynput
 
-sudo mkdir /etc/GPIOShortcuts
-sudo cp config.txt /etc/GPIOShortcuts/
-sudo cp service.py /etc/GPIOShortcuts/
-sudo cp GPIOShortcuts.service /lib/systemd/system/
-sudo chmod 644 /lib/systemd/system/GPIOShortcuts.service
-sudo chmod +x /etc/GPIOShortcuts/service.py
+ETC=/etc/GPIOShortcuts
+SYSTEMD=/lib/systemd/system/
+if [ ! -d $DIR ]; then
+    sudo mkdir $ETC
+fi
+sudo cp config.txt $ETC
+sudo cp service.py $ETC
+sudo cp GPIOShortcuts.service $SYSTEMD
+sudo cp GPIOrestart.* $SYSTEMD
+sudo chmod 644 $SYSTEMD/GPIOShortcuts.service
+sudo chmod 644 $SYSTEMD/GPIOrestart.*
+sudo chmod +x $ETC/service.py
 
 sudo systemctl daemon-reload
+
+sudo systemctl enable GPIOrestart.timer
 sudo systemctl enable GPIOShortcuts.service
-sudo systemctl stop GPIOShortcuts.service
-sudo systemctl start GPIOShortcuts.service
+sudo systemctl restart GPIOShortcuts.service
 
 echo "Done"
+
